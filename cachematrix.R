9077c6d490febaf3b8ce1aa2132fbd978293f056
Arjun Chavan <arjun.chavan999@gmail.com>
2020-08-09 18:54:29 +0530

    Completion of 2 caching functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To create 2 functions to get inverse of a matrix in a faster fashion
 
-## Write a short comment describing this function
-
+## This function makes a special "matrix" which caches the initial matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function checks if inverse exists in the earlier function, if not, generates one
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached inverse")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
 }
